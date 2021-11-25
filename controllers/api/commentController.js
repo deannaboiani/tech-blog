160892029cd48c562a7d18e64f003039c0dbcd55
DeAnna Boiani <deannaboiani@gmail.com>
2021-11-24 23:37:40 -0800

    finishes comment routes

--- controllers/api/commentController.js
+++ controllers/api/commentController.js
@@ -13,16 +13,33 @@ router.get("/",(req,res)=> {
   });
   });
 
-// router.post('/',  (req,res)=>{
-//      Comment.create(req.body)
-//      .then(newComment=>{
-//          res.json(newComment);
-//         })
-//       .catch (err=>{
-//           res.status(500).json(err);
-//       })
+router.post('/',  (req,res)=>{
+     Comment.create(req.body)
+     .then(newComment=>{
+         res.json(newComment);
+        })
+      .catch (err=>{
+          res.status(500).json(err);
+      })
       
-// });
+});
+
+// delete by id
+router.delete('/:id',  (req,res)=>{
+    Comment.destroy({
+        where: {
+            id:req.params.id
+        }
+    })
+    .then(delComment=>{
+        res.json(delComment);
+       })
+     .catch (err=>{
+         res.status(500).json(err);
+     })
+     
+});
+
 
 
 module.exports = router;
